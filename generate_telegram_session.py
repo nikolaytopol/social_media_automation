6fbf4cf4ec36bfc644dacdab159c8d78def2cdfa
Nikolay Topol <krasauucik@gmail.com>
2025-07-18 20:54:43 +0200

    Update generate_telegram_session.py

--- generate_telegram_session.py
+++ generate_telegram_session.py
@@ -1,9 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-# Define your API ID and API Hash here
-api_id = 7178098  # <-- Replace with your API ID
-api_hash = 'e5a435bec5c99532f1e019c3738eab8c'  # <-- Replace with your API Hash
 
 phone = input('Enter your phone number: ')
 
